8e6f371bff56e8388579667fec92d2d68e9c5453
Achala007 <achalayasas007@gmail.com>
2016-12-18 21:46:21 +0530

    true fals

--- Desktop/ebank 1/eBank/src/cstute/viewEmployeeList.java
+++ Desktop/ebank 1/eBank/src/cstute/viewEmployeeList.java
@@ -124,7 +124,7 @@ public class viewEmployeeList extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new viewEmployeeList().setVisible(true);
+                new viewEmployeeList().setVisible(false);
             }
         });
     }
