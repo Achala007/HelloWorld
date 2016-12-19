05ec04f46d37c164e99f71ad6ccfac93756b319c
Achala007 <achalayasas007@gmail.com>
2016-12-19 21:55:16 +0530

    logger change

--- Desktop/ebank 1/eBank/src/cstute/CSATute.java
+++ Desktop/ebank 1/eBank/src/cstute/CSATute.java
@@ -28,7 +28,9 @@ public class CSATute {
             java.util.logging.Logger.getLogger(Selections.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
             java.util.logging.Logger.getLogger(Selections.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } 
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Selections.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
 
         Login.start();
     }
