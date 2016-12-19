0963f5ac4c5b2f31384d1ee6b26db8fadfb26560
Achala007 <achalayasas007@gmail.com>
2016-12-19 21:58:43 +0530

    cus up3

--- Desktop/ebank 1/eBank/src/cstute/CustomerUp.java
+++ Desktop/ebank 1/eBank/src/cstute/CustomerUp.java
@@ -102,7 +102,12 @@ public class CustomerUp extends javax.swing.JFrame {
 
         jButton1.setText("jButton1");
 
-        setD
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        DOBTextField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DOBTextFieldActionPerformed(evt);
+            }
         });
 
         createCustomerButton.setText("Add Customer");
