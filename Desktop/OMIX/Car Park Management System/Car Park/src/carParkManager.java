d404219b6a6f40fdda2317a70fb4385383bbaa24
Achala007 <achalayasas007@gmail.com>
2017-04-21 10:46:02 +0530

    achala first commit

--- Desktop/OMIX/Car Park Management System/Car Park/src/carParkManager.java
+++
@@ -1,9 +0,0 @@
-
-public interface carParkManager {
-	public void addVehicle();
-	public void deleteVehicle();
-	public void List();
-	public void Stat();
-	public void MaxANDMin();
-	public void History();
-}
